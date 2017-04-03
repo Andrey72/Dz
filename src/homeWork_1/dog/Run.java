ab1ebc86915129ce407a01c7a6ceefb3913ea41a
Sherlock <vadsan27200@gmail.com>
2017-04-03 22:56:56 +0300

    some bad commit with idea

--- src/homeWork_1/dog/Run.java
+++ src/homeWork_1/dog/Run.java
@@ -3,6 +3,9 @@ package homeWork_1.dog;
 /**
  * Created by Sherlock on 03.04.2017.
  */
+
+/*2. Создать объект класса Щенок, используя классы Животное, Собака.
+Методы: вывести на консоль имя, подать голос, прыгать, бегать, кусать.*/
 public class Run {
     public static void main(String[] args) {
 
